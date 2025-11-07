d3b52b6b282eb21ea7caf0ba45910ef69f05a2fb
Clarissa McGhee <cm2259@cornell.edu>
2025-11-07 17:17:53 -0500

    first push

--- cyber-security-website/src/pages/About.tsx
+++ cyber-security-website/src/pages/About.tsx
@@ -5,7 +5,36 @@ const About = () => {
       <p className="font-['Roboto_Mono'] text-[#e1dad3] text-lg">
         Learn more about the club. Summary 
       </p>
+
+      <div>
+        <div className="flex gap-5 justify-center mt-5">
+          <div className="w-[500px] h-[200px] border-2 border-white p-[10px]">
+            <p> NME </p>
+          </div>
+          <div className="w-[500px] h-[200px] border-2 border-white p-[10px]">
+            <p> IME </p>
+          </div>
+        </div>
+        <div className="w-[500px] h-[200px] border-2 border-white p-[10px] ml-80 mt-10">
+          <p> Competition </p>
+        </div>
+        <div className="w-[500px] h-[200px] border-2 border-white p-[10px] ml-80 mt-10">
+          <p> What is a CTF? </p>
+        </div>
+        <div className="flex gap-5 justify-center mt-5">
+          <div className="w-[500px] h-[200px] border-2 border-white p-[10px]">
+            <p> CTF Team </p>
+          </div>
+          <div className="w-[500px] h-[200px] border-2 border-white p-[10px]">
+            <p> CCDC Team </p>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
+
   );
 };
 
