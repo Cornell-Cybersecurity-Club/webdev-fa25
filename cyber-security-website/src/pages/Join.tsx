d9b5fb46e45813ca351392335d4e87b5be5b81cc
labimn <9n.labi@gmail.com>
2025-11-14 16:10:15 -0500

    nhty

--- cyber-security-website/src/pages/Join.tsx
+++ cyber-security-website/src/pages/Join.tsx
@@ -1,9 +1,9 @@
 const Join = () => {
   return (
     <div className="text-center py-20 px-5">
-      <h1 className="font-['Roboto_Mono'] text-white text-5xl mb-5">become a cybear?
+      <h1 className="font-['Roboto_Mono'] text-white text-5xl mb-5">
+        become a cybear?
       </h1>
-     
     </div>
   );
 };
