1b4d87b2e94aaaf69284c902662fa05e4a633cd6
Clarissa McGhee <cmcghee987@gmail.com>
2026-02-18 19:18:33 -0500

    Add to people page

--- cyber-security-website/src/pages/People.tsx
+++ cyber-security-website/src/pages/People.tsx
@@ -10,7 +10,7 @@ const People = () => {
 
         <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5">
           <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
-            <p className="absolute -top-[15px] bg-[#000000] text-white w-[150px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[140px] text-md font-['Roboto_Mono'] pl-2 pr-2">
               {" "}
               Raman Khanal{" "}
             </p>
@@ -32,6 +32,10 @@ const People = () => {
                 className="w-[50px] h-[50px]"
                 /> 
             </div>
+            <p className="absolute bg-[#000000] text-white w-[100px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-50">
+              {" "}
+              President{" "}
+            </p>
           </div>
           <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
             <p className="absolute -top-[15px] bg-[#000000] text-white w-[170px] text-md font-['Roboto_Mono'] pl-2 pr-2">
@@ -56,7 +60,43 @@ const People = () => {
                 className="w-[50px] h-[50px]"
                 /> 
             </div>
+            <p className="absolute bg-[#000000] text-white w-[160px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-35">
+              {" "}
+              Vice President{" "}
+            </p>
           </div>
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[150px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Cam Mazzacane{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[100px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-50">
+              {" "}
+              Treasurer{" "}
+            </p>
+          </div>
+        </div>
+
+
+        <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-15">
           <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
             <p className="absolute -top-[15px] bg-[#000000] text-white w-[170px] text-md font-['Roboto_Mono'] pl-2 pr-2">
               {" "}
@@ -80,6 +120,185 @@ const People = () => {
                 className="w-[50px] h-[50px]"
                 /> 
             </div>
+            <p className="absolute bg-[#000000] text-white w-[100px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-50">
+              {" "}
+              Secretary{" "}
+            </p>
+          </div>
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[170px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Andrew Visconti{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[130px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-40">
+              {" "}
+              Recruitment{" "}
+            </p>
+          </div>
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[140px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Parth Mittal{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[210px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-20">
+              {" "}
+              New Member Education{" "}
+            </p>
+          </div>
+        </div>
+
+
+        <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-15">
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[135px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Bill Nguyen{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[140px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-40">
+              {" "}
+              Competitions{" "}
+            </p>
+          </div>
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[165px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Will Rosenthal{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[170px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-30">
+              {" "}
+              Public Relations{" "}
+            </p>
+          </div>
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[140px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Buster Coden{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[210px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-20">
+              {" "}
+              Events & Operations{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-15">
+          <div className="relative w-full max-w-[350px] h-[470px] border-2 border-white p-[10px] rounded-lg">
+            <p className="absolute -top-[15px] bg-[#000000] text-white w-[140px] text-md font-['Roboto_Mono'] pl-2 pr-2">
+              {" "}
+              Jack Britton{" "}
+            </p>
+            <img
+            src=""
+            alt="image here"
+            className="w-[150px] h-[180px] ml-20 mr-20 mt-20"
+            />
+            <p className="pl-15"> Major: </p>
+            <div className="flex flex-col md:flex-row gap-5 justify-center items-center mt-5"> 
+                <img
+                src="/mail_icon.jpg"
+                alt="mail"
+                className="w-[110px] h-[140px]"
+                />
+                <img
+                src="/link2_icon.avif"
+                alt="link icon here"
+                className="w-[50px] h-[50px]"
+                /> 
+            </div>
+            <p className="absolute bg-[#000000] text-white w-[210px] text-md font-['Roboto_Mono'] pl-2 pr-2 ml-20">
+              {" "}
+              Events & Operations{" "}
+            </p>
           </div>
         </div>
 
