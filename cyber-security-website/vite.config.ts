c2d6b8b99007e8ef4341ff872982f5e5b4f68463
PrivatePanda <parth@privatepanda.co>
2026-04-15 13:16:08 -0400

    Change base path in Vite configuration

--- cyber-security-website/vite.config.ts
+++ cyber-security-website/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/webdev-fa25/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
